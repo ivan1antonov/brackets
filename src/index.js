09062199dfca2d509fe5540715b6f0b9823fa3c2
ivan1antonov <ivan1antonov@gmail.com>
2023-10-27 01:08:59 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function insert (array){
+    for (let i = 0; i <= array.length; i++){
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if ((array[i] === bracketsConfig[j][0]) && (array[i+1] === bracketsConfig[j][1])){
+          array.splice(i, 2);
+          insert(array)
+        }        
+      }
+    }
+    if(array.length === 0) {
+      return true;
+    } else{
+      return false;
+    }    
+  }
+  return insert(str.split(''));
 }
